0a80f119c8b19ed2b0b16677bf86458880f60dee
muhammadderic <muhammad.donny.e@gmail.com>
2024-09-30 15:05:02 +0700

    Update user correction

--- utils/user_correction.py
+++ utils/user_correction.py
@@ -1,21 +1,18 @@
 import streamlit as st
 
 @st.dialog("Koreksi")
-def vote(item):
+def vote():
     reason = st.text_input("Masukkan kalimat yang benar")
     if st.button("Koreksi"):
         # Store the reason in session_state
-        st.session_state.vote = {"item": item, "reason": reason}
-        st.rerun()
+        st.session_state.vote = {"reason": reason}
+        return st.session_state.vote.get("reason")
 
 def show_correction_dialog():
     if "vote" not in st.session_state:
         st.write("Apakah kalimat koreksi sudah benar?")
         if st.button("👍"):
-            vote("right")
+            st.session_state.vote = {"reason": "Kalimat sudah benar"}
+            return st.session_state.vote.get("reason")
         if st.button("👎"):
-            vote("wrong")
-    else:
-        st.write("Terima kasih telah mengkoreksi!")
-        # Return the reason for further use
-        return st.session_state.vote.get("reason")
+            vote()
