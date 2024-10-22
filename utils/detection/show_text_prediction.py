b76aa98beb3090fb6c2b1257fa4bc149f16114f3
muhammadderic <muhammad.donny.e@gmail.com>
2024-10-22 14:02:30 +0700

    Update show text prediction function

--- utils/detection/show_text_prediction.py
+++ utils/detection/show_text_prediction.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def show_text_prediction(y_pred):
-    if 1 in y_pred:
+    if 1 in y_pred or y_pred == 1:
         text = f"⚠️ Kalimat ini mengandung kata/kata-kata yang salah"
         st.toast(text)
         return 1
