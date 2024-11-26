c528fa0ed5759dfe750f7591266df58a1cca0db2
muhammadderic <muhammad.donny.e@gmail.com>
2024-11-26 07:21:16 +0700

    Update

--- utils/add_correct_sentence.py
+++ utils/add_correct_sentence.py
@@ -7,19 +7,11 @@ def add_correct_sentence(kalimat_salah='default', kalimat_koreksi='default'):
     key = st.secrets["SUPABASE_KEY"]
     supabase: Client = create_client(url, key)
 
-    # Validate input
-    if not kalimat_salah.strip() or not kalimat_koreksi.strip():
-        st.error("Both 'kalimat_salah' and 'kalimat_koreksi' must be provided.")
-        return
+    # Insert data into Supabase
+    if st.button("Simpan"):
+        supabase.table("spelling_correction").insert({
+                            "kalimat_salah": kalimat_salah,
+                            "kalimat_benar": kalimat_koreksi
+                        }).execute()
 
-    try:
-        # Insert data into Supabase
-        if st.button("Simpan"):
-            supabase.table("spelling_correction").insert({
-                                "kalimat_salah": kalimat_salah,
-                                "kalimat_benar": kalimat_koreksi
-                            }).execute()
-
-            st.success("Data successfully inserted!")
-    except Exception as e:
-        st.error(f"Failed to insert data: {e}")
+        st.success("Data successfully inserted!")
