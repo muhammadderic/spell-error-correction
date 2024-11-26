dc549f20015b6bed874849ac7433b2087e24b83c
muhammadderic <muhammad.donny.e@gmail.com>
2024-11-26 08:05:16 +0700

    Update

--- app.py
+++ app.py
@@ -178,15 +178,17 @@ if submit:
             st.info(transformed_sentence)
 
             # with st.popover("Koreksi kalimat, jika kalimat salah", use_container_width=True):
-            with st.container():
+            # with st.container():
                 # Insert data into Supabase
-                correct_sentence = st.text_area("Kalimat yang benar menurut anda adalah:")
                 # Add a submit button
+                # correct_sentence = st.text_area("Kalimat yang benar menurut anda adalah:")
                 # if st.button("Simpan Kalimat Koreksi"):
                     # if correct_sentence.strip():  # Ensure input is not empty
                     #     st.success("Kalimat koreksi berhasil disimpan!")
                     # else:
                     #     st.warning("Mohon masukkan kalimat koreksi sebelum menyimpan.")
+            st.write("Silakan koreksi kalimat jika kalimat koreksi salah:")
+            correct_sentence = st.text_area("Kalimat yang benar menurut anda adalah:")
             add_correct_sentence(transformed_sentence, correct_sentence)
 
     else:
